64e730899ff858610c840fe77a72f33ae6964039
Tereza Novakova <60270092+terezano@users.noreply.github.com>
2020-03-04 17:56:07 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Jv49Hmi0sR_zarv-mRYRwu0MqEJavuumPYAwrTWaEq4/edit#gid=0';
