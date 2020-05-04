fa70047f4bf81114faca6d2dab1449fbede91e5c
eliskareg <36920970+eliskareg@users.noreply.github.com>
2020-05-04 19:38:56 +0200

    p

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -88,8 +88,9 @@ $(window).on('load', function() {
     var chapterContainerMargin = 70;
 
     document.title = getSetting('_mapTitle');
-    $('#title').append('<h2>PRŮVODCE</h2>');
+    $('#title').append('<p class="header-p">PRŮVODCE</p>');
     $('#title').append('<h3>' + getSetting('_mapTitle') + '</h3>');
+    $('#title').append('<p class="header-p">FESTIVALEM</p>');
     $('#title').append('<h4>' + getSetting('_mapSubtitle') + '</h4>');
     $('#title').append('<p class="main-p">' + getSetting('_mapParagraph1') + '</p>');
     $('#title').append('<p class="main-p">' + getSetting('_mapParagraph2') + '</p>');
